78f310382fd911857f0d876cd7281bb82404e3bd
Alan Shaw <alan@storacha.network>
2025-01-13 11:08:01 +0000

    docs: add comments explaining shard constants

--- shard/constants.go
+++ shard/constants.go
@@ -1,4 +1,9 @@
 package shard
 
+// KeyCharsASCII refers to printable ASCII characters in the code range 32-126.
 const KeyCharsASCII = "ascii"
+
+// MaxKeySize is a default maximum key size in bytes. It is the same as MAX_PATH
+// - the maximum filename+path size on most windows/unix systems, so should be
+// sufficient for most purposes.
 const MaxKeySize = 4096
